17e993504500c85c803a313359f0007f75db8faf
Gerson Cardoso Filho <gersoncfilho@mac.com>
2016-03-19 19:42:57 -0300

    Mudanças no layout

--- platforms/android/assets/www/js/app.module.js
+++ platforms/android/assets/www/js/app.module.js
@@ -1,1 +1,8 @@
-var app = angular.module("GCProtoApp",["ui-listView"]);
+var app = angular.module("GCProtoApp",['ui-listView', 'uiGmapgoogle-maps'])
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({        
+        key: 'AIzaSyAimn8YL1R383fSyjL-zpTShSt1_cqqfu8', //    key: 'your api key',
+        v: '3.23', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+});

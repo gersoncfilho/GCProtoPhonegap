365aa7b9ed639d92acfd9abe8660a2b6a21e6342
Gerson Cardoso Filho <gersoncfilho@mac.com>
2016-03-20 20:00:33 -0300

    Criação da view agendar

--- platforms/android/assets/www/js/main.module.js
+++ platforms/android/assets/www/js/main.module.js
@@ -16,6 +16,10 @@ App.config(['$routeProvider',
         templateUrl: 'templates/favoritos.html',
         controller: 'FavoritosController'
       }).
+      when('/Agenda', {
+        templateUrl: 'templates/agenda.html',
+        controller: 'AgendaController'
+      }).
       otherwise({
         redirectTo: '/Notificacoes'
       })
@@ -227,61 +231,8 @@ App.controller('FavoritosController', function($scope) {
 ];
 
 });
-App.controller('agendaController', function($scope) {
+App.controller('AgendaController', function($scope) {
  
-    $scope.titulo = 'Agenda';
- 
-    $scope.agenda = [
-      {
-        'nome':'Dr. José da Silva',
-        'especialidade':'Cardiologista',
-        'data':'18 de março de 2016',
-        'horario':'10:00 hs',
-        'endereco1':'Norteshopping - Centro Médico',
-        'endereco2':'Av. Dom Hélder Câmara, 4000',
-        "endereco3":"Bloco 4 - Sala 1313",
-        'tel':'2689-0909',
-        'email':'joseferreira@consultorio.com.br',
-        'latitude':'-22.9135455',
-        'longitude':'-43.2235144'
-      },
-      {
-        'nome':'Dr. Pedro Ricardo',
-        'especialidade':'Nutricionista',
-        'endereco1':'Barrashopping - Centro Médico',
-        'endereco2':'Av. das Américas, 5000',
-        "endereco3":"Bloco 2 - Sala 3131",
-        'tel':'8899-9999',
-        'email':'joseferreira@consultorio.com.br',
-        'latitude':'-22.9135455',
-        'longitude':'-43.2235144'
-      },
-      {
-        'nome':'Dr. José da Silva',
-        'especialidade':'Cardiologista',
-        'data':'18 de março de 2016',
-        'horario':'10:00 hs',
-        'endereco1':'Norteshopping - Centro Médico',
-        'endereco2':'Av. Dom Hélder Câmara, 4000',
-        "endereco3":"Bloco 4 - Sala 1313",
-        'tel':'2689-0909',
-        'email':'joseferreira@consultorio.com.br',
-        'latitude':'-22.9135455',
-        'longitude':'-43.2235144'
-      },
-      {
-        'nome':'Dr. José da Silva',
-        'especialidade':'Cardiologista',
-        'data':'18 de março de 2016',
-        'horario':'10:00 hs',
-        'endereco1':'Norteshopping - Centro Médico',
-        'endereco2':'Av. Dom Hélder Câmara, 4000',
-        "endereco3":"Bloco 4 - Sala 1313",
-        'tel':'2689-0909',
-        'email':'joseferreira@consultorio.com.br',
-        'latitude':'-22.9135455',
-        'longitude':'-43.2235144'
-      }
-];
+    $scope.titulo = 'Agendar';
 
 });
